359b2984c4ffe3b590ae1ddb8a931f833ea21d2f
sari705 <sari125705@gmail.com>
2024-12-06 11:53:31 +0200

    adding main function

--- project 2/src/Main.java
+++ project 2/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        TaskManagement management = new TaskManagement();
+
+        management.AddTask(new Task("fixing", 2));
+        management.AddTask(new Task("baking", 2));
+        management.AddTask(new Task("washing", 1));
+        management.AddTask(new Task("eating", 3));
+        management.AddTask(new Task("drinking", 1));
+        management.AddTask(new Task("looking", 3));
+        management.AddTask(new Task("running", 3));
+        management.AddTask(new Task("swimming", 2));
+        management.AddTask(new Task("drawing", 1));
+
+        System.out.println(management.toString());
 
 
     }
